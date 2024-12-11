cf8ca30c6273697ef12a95335b6fee4031bc6a54
zap8600 <95487746+zap8600@users.noreply.github.com>
2024-12-11 17:57:37 +0000

    wip no clue what im doing T-T

--- main.cpp
+++ main.cpp
@@ -1,7 +1,30 @@
 #include <iostream>
 #include <complex>
+#include <vector>
 
 using namespace std::complex_literals;
+
+class Matrix {
+    public:
+        std::vector<std::complex<double>> m;
+        long rows;
+        long columns;
+
+        Matrix(long nr, long nc) {
+            rows = nr;
+            columns = nc;
+        }
+
+        void Identity() {
+            for(long j = 0; j < rows; j++) {
+                m[(j * rows) + j] = 1.0;
+            }
+        }
+
+        Matrix operator*(Matrix const& right) {
+            Matrix result(rows, columns);
+        }
+};
 
 int main() {
     return 0;
