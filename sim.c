2216fadd2feabcfd0e4a176d31fc298d8247e699
zap8600 <95487746+zap8600@users.noreply.github.com>
2024-12-12 09:27:26 -0600

    wip

---
+++ sim.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <math.h>
+#include <complex.h>
+// Simulate a quantum computer
+// Based on [qc64](https://github.com/dakk/qc64)
+
+// r* are real parts and i* are imagenary parts of the sv
+// TODO: Use complex numbers
+double r0 = 1.0; double i0 = 0.0; double r1 - 0.0; double i1 = 0.0;
+double r2 = 0.0; double i2 = 0.0; double r3 = 0.0; double i3 = 0.0;
+double a0 = 0.0;
+double shots = 28.0;
+
+int main() {
+    return 0;
+}
