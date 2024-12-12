457fc23a803535818618c0addf1d6279a5ba5b0a
zap8600 <95487746+zap8600@users.noreply.github.com>
2024-12-12 12:59:40 -0600

    wip

--- sim.c
+++ sim.c
@@ -10,18 +10,23 @@ double complex num0 = 1.0;
 double complex num1 = 0.0;
 double complex num2 = 0.0;
 double complex num3 = 0.0;
+double complex a0 = 0.0;
 
 void x0() {
-    double complex tmp = num0;
+    a0 = num0;
     num0 = num1;
-    num1 = tmp;
+    num1 = a0;
 
-    tmp = num2;
+    a0 = num2;
     num2 = num3;
-    num3 = tmp;
+    num3 = a0;
 }
 
 int main() {
     printf("Running X0 gate...\n");
+    double sq = pow(creal(num0), 2.0) + pow(cimag(num0), 2) + pow(creal(num1), 2) + pow(cimag(num1), 2) + pow(creal(num2), 2) + pow(cimag(num2), 2) + pow(creal(num3), 2) + pow(cimag(num3), 2);
+    if(abs(sq - 1) > 0.00001) {
+        double nf = sqrt(1 / sq);
+    }
     return 0;
 }
